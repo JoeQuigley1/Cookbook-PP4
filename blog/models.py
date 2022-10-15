ab78d475f695f1a1ea8e5027f5f34ca2445d2269
JoeQuigley1 <joeyjtq@hotmail.com>
2022-10-15 21:32:16 +0000

    Add functionality to admin panel

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,8 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -21,7 +22,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_on']
-
 
     def __str__(self):
         return self.title
