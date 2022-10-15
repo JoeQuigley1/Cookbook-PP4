17a91e96cb003ca66ec8117ea135c5decb923c0f
JoeQuigley1 <joeyjtq@hotmail.com>
2022-10-15 20:32:09 +0000

    Add comment class to models

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,21 @@ class Post(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    name = moedls.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+
+    class Meta:
+        ordering = ['created_on']
+
+
+    def __str__(self):
+        return f"Comment {self.body } by {self.name}"
