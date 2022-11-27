a2a1835e3735ff7e7fc7a3b40dbfc6f152c1460a
JoeQuigley1 <joeyjtq@hotmail.com>
2022-11-27 18:51:13 +0000

    Add about page and wire to base and add stock image

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,11 @@ from .models import Post
 from .forms import CommentForm
 
 
+def about_page(request):
+
+    return render(request, 'about.html')
+    
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
